c892e44857982f064e25b92d7c1cd7827e91585a
djosix <djosicks@gmail.com>
2018-12-02 10:48:41 +0800

    fix locale

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -7,10 +7,10 @@ test -d ~/.bin && PATH=~/.bin:$PATH
 
 
 # locales
+export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
-export LC_CTYPE=UTF-8
 
 
 # aliases
